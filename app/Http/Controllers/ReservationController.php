e9aca05e4df09e39927fee675a16bbe69db5e802
BleuSkell <kyasowi1@gmail.com>
2025-05-27 11:47:40 +0200

    started with create and index

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('reservations.index');
+    }
+
+    public function create()
+    {
+        return view('reservations.create');
+    }
 }
