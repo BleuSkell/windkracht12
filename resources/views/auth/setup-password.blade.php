4dbfca604fbb3f1aeafa303aa95d13906e5666b8
BleuSkell <kyasowi1@gmail.com>
2025-05-30 21:25:40 +0200

    styled registration, login, password setup views

--- resources/views/auth/setup-password.blade.php
+++ resources/views/auth/setup-password.blade.php
@@ -3,18 +3,28 @@
         @csrf
 
         <div>
-            <x-input-label for="password" value="Wachtwoord" />
-            <x-text-input id="password" type="password" name="password" required autofocus />
+            <x-input-label for="password" value="Wachtwoord instellen" />
+            <x-text-input id="password" class="block mt-1 w-full"
+                            type="password" 
+                            name="password" 
+                            required autofocus
+            />
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <div class="mt-4">
             <x-input-label for="password_confirmation" value="Bevestig wachtwoord" />
-            <x-text-input id="password_confirmation" type="password" name="password_confirmation" required />
+            <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                            type="password" 
+                            name="password_confirmation" 
+                            required
+            />
+            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="mt-4">
-            <x-primary-button>
-                Stel wachtwoord in
+        <div class="flex items-center justify-end mt-4">
+            <x-primary-button class="ms-3">
+                {{ __('Registreer') }}
             </x-primary-button>
         </div>
     </form>
